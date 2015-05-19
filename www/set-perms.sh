c72ded0f24eb421879ee9b6f5ddd94f8eea49f60
Anton Staroverov <by@tonystar.me>
2015-05-19 13:19:25 +0300

    Fix owning group to vagrant

--- www/set-perms.sh
+++ www/set-perms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-chown -R vagrant:www-data /srv/www/$1
+chown -R vagrant:vagrant /srv/www/$1
 find /srv/www/$1 -type d -exec chmod 755 {} +
 find /srv/www/$1 -type f -exec chmod 644 {} +
 find /srv/www/$1/sites/default/files -type d -exec chmod 775 {} +
