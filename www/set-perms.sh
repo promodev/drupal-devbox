3143157fccb62add3d5a26d9a49982b9ed1e70ea
Anton Staroverov <by@tonystar.me>
2015-05-19 18:00:19 +0300

    Improve set-perms.sh

--- www/set-perms.sh
+++ www/set-perms.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-chown -R vagrant:vagrant /srv/www/$1
-find /srv/www/$1 -type d -exec chmod 755 {} +
+chown -R vagrant:www-data /srv/www/$1
+chmod -R ug-s /srv/www/$1
+find /srv/www/$1 -type d -exec chmod 2755 {} +
 find /srv/www/$1 -type f -exec chmod 644 {} +
 find /srv/www/$1/sites/default/files -type d -exec chmod 775 {} +
 find /srv/www/$1/sites/default/files -type f -exec chmod 664 {} +
