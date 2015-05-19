f3d51a8447af7c849280aaafd3e8c3cd70cdd3a7
Anton Staroverov <by@tonystar.me>
2015-05-19 21:28:10 +0300

    Fix new file permissions during deploy

--- www/deploy.sh
+++ www/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+sudo ./fix-chmod.sh $1
 drush @$1-dev vset --exact maintenance_mode 1
 drush @$1-dev cc all
 drush @$1-live vset --exact maintenance_mode 1
 drush @$1-live cc all
 drush @$1-live arb
-drush rsync @$1-dev @$1-live --mode=rlptD --delete
+drush rsync @$1-dev @$1-live --mode=rltD --delete --chmod=ugo=rwX
 drush sql-sync @$1-dev @$1-live
 drush @$1-live vset --exact maintenance_mode 0
---
+++ www/fix-chown.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+chown -R vagrant:www-data /srv/www/$1
