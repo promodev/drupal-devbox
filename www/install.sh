a55f36b71dba6bad8eb6ef44240576d5b0489c90
Anton Staroverov <by@tonystar.me>
2015-05-20 09:12:18 +0300

    Use admin theme for content editing

--- www/install.sh
+++ www/install.sh
@@ -5,6 +5,7 @@ drush dl drupal --drupal-project-rename=$1
 cd $1
 drush -y si minimal --db-url=mysql://$1:$1@localhost/$1 --account-pass=$1
 drush vset admin_theme seven
+drush vset node_admin_theme 1
 drush dl admin_menu
 drush -y en admin_menu_toolbar
 cd ../
