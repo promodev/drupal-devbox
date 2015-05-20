46e9d1d36d9f074afebacb2febe4541d79209e54
Anton Staroverov <by@tonystar.me>
2015-05-20 09:10:01 +0300

    Install Admin Menu by default

--- www/install.sh
+++ www/install.sh
@@ -2,5 +2,9 @@
 set -e
 
 drush dl drupal --drupal-project-rename=$1
-cd $1 && drush -y si minimal --db-url=mysql://$1:$1@localhost/$1 --account-pass=$1
+cd $1
+drush -y si minimal --db-url=mysql://$1:$1@localhost/$1 --account-pass=$1
+drush vset admin_theme seven
+drush dl admin_menu
+drush -y en admin_menu_toolbar
 cd ../
