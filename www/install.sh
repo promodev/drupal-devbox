ee448dc7157aa3c12fde9451c0bb32c3e0ae73de
Anton Staroverov <by@tonystar.me>
2015-05-24 13:28:30 +0300

    Improve install.sh

--- www/install.sh
+++ www/install.sh
@@ -3,9 +3,9 @@ set -e
 
 drush dl drupal --drupal-project-rename=$1
 cd $1
-drush -y si minimal --db-url=mysql://$1:$1@localhost/$1 --account-pass=$1
+drush -y si minimal --db-url=mysql://$1:$1@localhost/$1 --account-name=$1 --account-pass=$1
 drush vset admin_theme seven
 drush vset node_admin_theme 1
-drush dl admin_menu
-drush -y en admin_menu_toolbar
+drush dl admin_menu readonlymode devel
+drush -y en admin_menu_toolbar readonlymode
 cd ../
